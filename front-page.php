1388807f136993e9523ffb4383aab6a31b49f6cd
Caitlin MacMinn <macminn.caitlin@gmail.com>
2016-07-27 13:29:18 -0400

    making all dem changes

--- front-page.php
+++ front-page.php
@@ -8,15 +8,13 @@ if(have_posts()) {
 		the_post();
 ?>
 
-<?php $heroImage = get_field('hero_image'); ?>
-<header id='home' style='background: linear-gradient(45deg, rgba(30,159,180,0.15) 0%,rgba(30,159,180,0.25) 100%), url("<?php echo $heroImage['url'] ?>") center center no-repeat; background-size: cover;'>
-
+<header id='home'>
 	<nav class='mainNav' id='mainNav'>
 		<div class="fa fa-bars fa-2x"></div>
 		<section class='container'>
 			<div class='row'>
 				<div class='three columns'>
-					<h3><?php the_field('business_name'); ?></h3>
+					<h3><?php the_field('menu_business_name'); ?></h3>
 				</div>
 				<div class='nine columns'>
 					<?php wp_nav_menu( array(
@@ -30,11 +28,11 @@ if(have_posts()) {
 	<section class='heroText'>
 		<section class='container'>
 			<div class='row'>
-				<h1 class=''><?php the_field('header_hero_text'); ?></h1>
-				<h4 class=''><?php the_field('header_tagline'); ?></h4>
+				<h1>We Are <span><?php the_field('hero_business_name'); ?></span></h1>
+				<h4>A Full Service Creative Digital Agency</h4>
 				<div class='buttonContainer'>
 					<?php $url = site_url('/#about', 'http'); ?>
-					<a href='<?php echo $url; ?>'><button><?php the_field('button_content'); ?></button></a>
+					<a href='<?php echo $url; ?>'><button>Learn More</button></a>
 				</div>
 			</div>
 		</section>
@@ -42,16 +40,15 @@ if(have_posts()) {
 </header>
 
 <!-- ABOUT SECTION -->
-<?php $aboutImage = get_field('about_background_image'); ?>
-<section class='about' id='about' style='background: radial-gradient(circle at 65%,rgba(135,170,175,0.7) 15%,rgba(114,168,176,0.9) 30%,#1e9fb4 60%), url("<?php echo $aboutImage['url'] ?>") center center no-repeat; background-size: cover;'>
+<section class='about' id='about'>
 	<section class='container'>
 		<div class='sectionTitle'>
-			<h2><?php the_field('about_title'); ?></h2>
+			<h2>About Us</h2>
 			<hr>
 		</div>
 		<section class='aboutContainer row'>
 			<div class='six columns'>
-				<h3><?php the_field('about_tagline'); ?></h3>
+				<h3>We Love to Create Beautiful Digital Experiences</h3>
 				<p><?php the_field('about_content'); ?></p>
 			</div>
 		</section> <!-- close .aboutContainer -->
@@ -63,29 +60,29 @@ if(have_posts()) {
 <section class='services' id='services'> 
 	<section class='container'> 
 		<div class='sectionTitle'>
-			<h2><?php the_field('services_title'); ?></h2>
+			<h2>Our Services</h2>
 			<hr>
 		</div>
 		<section class='servicesContainer row'> 
 			<div class='servicesContent three columns'>
-				<span><?php the_field('service_1_icon'); ?></span>
-				<h4><?php the_field('service_1_title'); ?></h4>
-				<p><?php the_field('service_1_description'); ?></p>
+				<span><?php the_field('service:_design_icon'); ?></span>
+				<h4>Design</h4>
+				<p><?php the_field('service:_design_description'); ?></p>
 			</div>
 			<div class='servicesContent three columns'>
-				<span><?php the_field('service_2_icon'); ?></span>
-				<h4><?php the_field('service_2_title'); ?></h4>
-				<p><?php the_field('service_2_description'); ?></p>
+				<span><?php the_field('service:_development_icon'); ?></span>
+				<h4>Development</h4>
+				<p><?php the_field('service:_development_description'); ?></p>
 			</div>
 			<div class='servicesContent three columns'>
-				<span><?php the_field('service_3_icon'); ?></span>
-				<h4><?php the_field('service_3_title'); ?></h4>
-				<p><?php the_field('service_3_description'); ?></p>
+				<span><?php the_field('service:_ecommerce_icon'); ?></span>
+				<h4>Ecommerce</h4>
+				<p><?php the_field('service:_ecommerce_description'); ?></p>
 			</div>
 			<div class='servicesContent three columns'>
-				<span><?php the_field('service_4_icon'); ?></span>
-				<h4><?php the_field('service_4_title'); ?></h4>
-				<p><?php the_field('service_4_description'); ?></p>
+				<span><?php the_field('service:_branding_icon'); ?></span>
+				<h4>Branding</h4>
+				<p><?php the_field('service:_branding_description'); ?></p>
 			</div>
 		</section> <!-- close .servicesContainer -->
 	</section> <!-- close .container -->
@@ -96,7 +93,7 @@ if(have_posts()) {
 <section class='portfolio' id='portfolio'>
 	<section class='container'>
 		<div class='sectionTitle'>
-			<h2><?php the_field('portfolio_title'); ?></h2>
+			<h2>Our Portfolio</h2>
 			<hr>
 		</div>
 		<section class='portfolioContainer row'>
@@ -134,7 +131,7 @@ if(have_posts()) {
 <section class='team' id='team'>
 	<section class='container'>
 		<div class='sectionTitle'>
-			<h2><?php the_field('team_title'); ?></h2>
+			<h2>Our Team</h2>
 			<hr>
 		</div>
 		<section class='teamContainer row'> 
@@ -204,80 +201,40 @@ if(have_posts()) {
 <!-- TEAM ENDS -->
 
 <!-- COUNTER SECTION -->
-<section class='counter'> 
+<section class='achievements'> 
 	<section class='container'>
 		<section class='row'> 
-			<div class='counterContent three columns'>
-				<span><?php the_field('counter_1_number'); ?></span>
-				<h4><?php the_field('counter_1_title'); ?></h4>
+			<div class='achievementsContent three columns'>
+				<span><?php the_field('achievement:_number_of_happy_clients'); ?></span>
+				<h4>Happy Clients</h4>
 			</div>
-			<div class='counterContent three columns'>
-				<span><?php the_field('counter_2_number'); ?></span>
-				<h4><?php the_field('counter_2_title'); ?></h4>
+			<div class='achievementsContent three columns'>
+				<span><?php the_field('achievement:_number_of_completed_projects'); ?></span>
+				<h4>Projects Completed</h4>
 			</div>
-			<div class='counterContent three columns'>
-				<span><?php the_field('counter_3_number'); ?></span>
-				<h4><?php the_field('counter_3_title'); ?></h4>
+			<div class='achievementsContent three columns'>
+				<span><?php the_field('achievement:_number_of_awards_won'); ?></span>
+				<h4>Awards Won</h4>
 			</div>
-			<div class='counterContent three columns'>
-				<span><?php the_field('counter_4_number'); ?></span>
-				<h4><?php the_field('counter_4_title'); ?></h4>
+			<div class='achievementsContent three columns'>
+				<span><?php the_field('achievement:_years_of_experience'); ?></span>
+				<h4>Years Experience</h4>
 			</div>
 		</section> <!-- close .counterContainer -->
 	</section> <!-- close .container -->
 </section> <!-- close .counter -->
 <!-- COUNTER ENDS -->
 
-<!-- TESTIMONIALS SECTION -->
-<section class='testimonials' id='testimonials'>
-	<section class='container'>
-		<div class='sectionTitle'>
-			<h2><?php the_field('testimonials_title'); ?></h2>
-			<hr>
-		</div>
-		<section class='testimonialsContainer row'> 
-			<div class='testimonialsContent four columns'>
-				<blockquote><p><?php the_field('testimonial_1_content'); ?></p></blockquote>
-				<?php $endorser1 = get_field('testimonial_1_endorser_image'); ?>
-				<div class='endorserContent'>
-					<img src="<?php echo $endorser1['url']?>" />
-					<h6><?php the_field('testimonial_1_endorser_name'); ?></h6>
-					<p><?php the_field('testimonial_1_endorser_job_title'); ?></p>
-				</div>
-			</div>
-			<div class='testimonialsContent four columns'>
-				<blockquote><p><?php the_field('testimonial_2_content'); ?></p></blockquote>
-				<?php $endorser2 = get_field('testimonial_2_endorser_image'); ?>
-				<div class='endorserContent'>
-					<img src="<?php echo $endorser2['url']?>" />
-					<h6><?php the_field('testimonial_2_endorser_name'); ?></h6>
-					<p><?php the_field('testimonial_2_endorser_job_title'); ?></p>
-				</div>
-			</div>
-			<div class='testimonialsContent four columns'>
-				<blockquote><p><?php the_field('testimonial_3_content'); ?></p></blockquote>
-				<?php $endorser3 = get_field('testimonial_3_endorser_image'); ?>
-				<div class='endorserContent'>
-					<img src="<?php echo $endorser3['url']?>" />
-					<h6><?php the_field('testimonial_3_endorser_name'); ?></h6>
-					<p><?php the_field('testimonial_3_endorser_job_title'); ?></p>
-				</div>
-			</div>
-		</section> <!-- close .testimonialsContainer -->
-	</section> <!-- close .container -->
-</section> <!-- close .testimonials -->
-<!-- TESTIMONIALS ENDS -->
-
 <!-- CONTACT SECTION -->
 <section class='contact' id='contact'>
 	<section class='container'>
 		<div class='sectionTitle'>
-			<h2><?php the_field('contact_title'); ?></h2>
+			<h2>Contact Us</h2>
 			<hr>
 		</div>
 		<section class='contactContainer row'>
 			<aside class='contactInfo four columns'>
-				<h3><?php the_field('contact_info_title'); ?></h3>
+				<h3>Contact Info</h3>
 				<div class=''>
 					<span>
 						<div class='icon'><i class="fa fa-map-marker fa-2x" aria-hidden="true"></i></div>
@@ -294,20 +251,20 @@ if(have_posts()) {
 				</div>
 			</aside>
 			<section class='contactForm eight columns'>
-				<h3><?php the_field('contact_form_title'); ?></h3>
+				<h3>Send Us A Message</h3>
 				<form action="http://www.focuspocus.io/magic/72feebeb74fc2c5eba3f55941579214a" method="POST">
-				<div class='formInputs row'>
-					<div class='six columns'>
-						<input type="text" name="Name" placeholder="Name">
+					<div class='formInputs row'>
+						<div class='six columns'>
+							<input type="text" name="Name" placeholder="Name">
+						</div>
+						<div class='six columns'>
+							<input type="email" name="Email Address" placeholder="Email">
+						</div>
 					</div>
-					<div class='six columns'>
-						<input type="email" name="Email Address" placeholder="Email">
+					<div class="formMessage row"> 
+						<textarea cols="30" rows="4" name="Message" placeholder="Message"></textarea>
+						<input type="submit" value="Send Message">
 					</div>
-				</div>
-				<div class="formMessage row"> 
-					<textarea cols="30" rows="4" name="Message" placeholder="Message"></textarea>
-					<input type="submit" value="Send Message">
-				</div>
 				</form>
 			</section>
 		</section> <!-- close .contactContainer -->
